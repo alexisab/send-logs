f78a6c9e5039466aa43ecbfe54fd0bcdf1d014a5
Alexis Ablain <alexis.ablain@gmail.com>
2019-01-14 10:35:51 +0100

    Script

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const CWLogsWritable = require('cwlogs-writable')
+
+const [groupName, baseName, accessKey, secretKey] = process.argv.slice(2)
+
+if (!groupName || !baseName || !accessKey || !secretKey) {
+    console.log('All flags are required.')
+    process.exit(1)
+}
+
+const streamName =
+    baseName +
+    '-' +
+    Date.now() +
+    '/' +
+    Math.round(Math.random() * 4026531839 + 268435456).toString(16)
+
+const cwstream = new CWLogsWritable({
+    logGroupName: groupName,
+    logStreamName: streamName,
+
+    cloudWatchLogsOptions: {
+        region: 'eu-west-3',
+
+        accessKeyId: accessKey,
+        secretAccessKey: secretKey,
+    },
+})
+
+process.stdin.setEncoding('utf8')
+
+process.stdin.pipe(cwstream)
+process.stdin.pipe(process.stdout)
+process.once('SIGINT', () => {})
+process.once('SIGTERM', () => {})
+
+console.log(process.argv)
